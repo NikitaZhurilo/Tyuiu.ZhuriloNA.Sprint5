def43a2b352aff98b56501688bf72b4d95d4f555
Tyuiu.ZhuriloNA@outlook.com <121nikit121@gmail.com>
2024-10-28 23:02:08 +0500

    Task0

--- Tyuiu.ZhuriloNA.Sprint5.Task0.V30.Lib/DataService.cs
+++ Tyuiu.ZhuriloNA.Sprint5.Task0.V30.Lib/DataService.cs
@@ -5,10 +5,10 @@ namespace Tyuiu.ZhuriloNA.Sprint5.Task0.V30.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            string filePath = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
-            double y = (x * x + 1) / (3 * x + 4);
+            double y = (Math.Pow(x, 2) + 1) / (3 * x + 4);
             y = Math.Round(y, 3);
-            File.WriteAllText(filePath, Convert.ToString(y));
+            string filePath = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
+            File.WriteAllText(filePath, y.ToString());
             return filePath;
         }
     }
--- Tyuiu.ZhuriloNA.Sprint5.Task0.V30.Test/DataServiceTest.cs
+++ Tyuiu.ZhuriloNA.Sprint5.Task0.V30.Test/DataServiceTest.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.ZhuriloNA.Sprint5.Task0.V30.Test
         [TestMethod]
         public void TestMethod1()
         {
-            string path = @"C:\Users\azhur\source\repos\Tyuiu.ZhuriloNA.Sprint5\Tyuiu.ZhuriloNA.Sprint5.Task0.V30\bin\Debug\net8.0\OutPutFileTask0.txt";
+            string path = @"C:\Users\azhur\AppData\Local\Temp\OutPutFileTask0.txt";
             FileInfo fileInfo = new FileInfo(path); 
             bool fileExists = fileInfo.Exists;
             bool wait = true;
