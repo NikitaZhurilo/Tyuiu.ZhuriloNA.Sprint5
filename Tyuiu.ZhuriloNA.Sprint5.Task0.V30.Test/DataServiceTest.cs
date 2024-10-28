195f4dcba59a76ca526cf913b524d9601df48ba0
Tyuiu.ZhuriloNA@outlook.com <121nikit121@gmail.com>
2024-10-28 22:36:06 +0500

    Add project files.

---
+++ Tyuiu.ZhuriloNA.Sprint5.Task0.V30.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+using Tyuiu.ZhuriloNA.Sprint5.Task0.V30.Lib;
+namespace Tyuiu.ZhuriloNA.Sprint5.Task0.V30.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\azhur\source\repos\Tyuiu.ZhuriloNA.Sprint5\Tyuiu.ZhuriloNA.Sprint5.Task0.V30\bin\Debug\net8.0\OutPutFileTask0.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.ZhuriloNA.Sprint5.Task0.V30/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.ZhuriloNA.Sprint5.Task0.V30.Lib;
+namespace Tyuiu.ZhuriloNA.Sprint5.Task0.V30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил Журило Н. А. | ИБКСб-24-1";
+            Console.WriteLine("*******************************************************************************");
+            Console.WriteLine("* Спринт #5                                                                   *");
+            Console.WriteLine("* Тема: Базовые навыки работы в с#                                            *");
+            Console.WriteLine("* Задание #1                                                                  *");
+            Console.WriteLine("* Вариант #30                                                                 *");
+            Console.WriteLine("* Выполнил Журило Никита Александрович | ИБКСб-24-1                           *");
+            Console.WriteLine("*******************************************************************************");
+            Console.WriteLine("* Условие                                                                     *");
+            Console.WriteLine("* Дано выражение вычислить его значение при x = 3, результат сохранить        *");
+            Console.WriteLine("* в текстовый файл OutPutFileTask0.txt и вывести на консоль. Округлить до     *");
+            Console.WriteLine("* трёх знаков после запятой                                                   *");
+            Console.WriteLine("*******************************************************************************");
+            Console.WriteLine("* Исходные данные:                                                            *");
+            Console.WriteLine("*******************************************************************************");
+            Console.WriteLine("x = " + x);
+            Console.WriteLine("* Результат:                                                                  *");
+            Console.WriteLine("*******************************************************************************");
+            string res = ds.SaveToFileTextData(x);
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан");
+            Console.ReadKey();
+        }
+    }
+}
