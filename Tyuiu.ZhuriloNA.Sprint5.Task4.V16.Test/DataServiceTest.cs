053eacf49d8dd21157ee3445de0bbc74691b789f
Tyuiu.ZhuriloNA@outlook.com <121nikit121@gmail.com>
2024-10-29 19:42:11 +0500

    Task4

--- Tyuiu.ZhuriloNA.Sprint5.Task4.V16.Test/DataServiceTest.cs
+++ Tyuiu.ZhuriloNA.Sprint5.Task4.V16.Test/DataServiceTest.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.ZhuriloNA.Sprint5.Task4.V16.Test
         [TestMethod]
         public void TestMethod1()
         {
-            string path = @"C:\Users\azhur\AppData\Local\Temp\InPutFileTask4.txt";
+            string path = @"C:\DataSprint5\InPutFileTask4.txt";
             FileInfo fileInfo = new FileInfo(path);
             bool fileExists = fileInfo.Exists;
             bool wait = true;
