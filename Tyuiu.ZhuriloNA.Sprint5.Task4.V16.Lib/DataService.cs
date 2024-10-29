32499ee2f0ef2ec18c2a88a2ecfb916cff4e72a3
Tyuiu.ZhuriloNA@outlook.com <121nikit121@gmail.com>
2024-10-30 01:38:15 +0500

    Task4

--- Tyuiu.ZhuriloNA.Sprint5.Task4.V16.Lib/DataService.cs
+++ Tyuiu.ZhuriloNA.Sprint5.Task4.V16.Lib/DataService.cs
@@ -8,7 +8,7 @@ namespace Tyuiu.ZhuriloNA.Sprint5.Task4.V16.Lib
         {
             string strX = File.ReadAllText(path);
             double res = Math.Cos(Convert.ToDouble(strX)) + Math.Pow(Convert.ToDouble(strX), 2) - (2 * Convert.ToDouble(strX)) / 1.2;
-            res = Math.Round(res, 3);
+            res  = Math.Round(res, 3);
             return res;
         }
     }
