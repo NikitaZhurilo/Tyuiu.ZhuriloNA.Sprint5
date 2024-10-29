18adac23244f1e19de73125787916940c616c47e
Tyuiu.ZhuriloNA@outlook.com <121nikit121@gmail.com>
2024-10-29 19:05:52 +0500

    Task3

---
+++ Tyuiu.ZhuriloNA.Sprint5.Task3.V15/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.ZhuriloNA.Sprint5.Task3.V15.Lib;
+namespace Tyuiu.ZhuriloNA.Sprint5.Task3.V15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил Журило Н. А. | ИБКСб-24-1";
+            Console.WriteLine("*******************************************************************************");
+            Console.WriteLine("* Спринт #5                                                                   *");
+            Console.WriteLine("* Тема: Базовые навыки работы в с#                                            *");
+            Console.WriteLine("* Задание #4                                                                  *");
+            Console.WriteLine("* Вариант #15                                                                 *");
+            Console.WriteLine("* Выполнил Журило Никита Александрович | ИБКСб-24-1                           *");
+            Console.WriteLine("*******************************************************************************");
+            Console.WriteLine("* Условие                                                                     *");
+            Console.WriteLine("* Дано выражение , вычислить его значение при x = 5, результат сохранить      *");
+            Console.WriteLine("* в бинарный файл OutPutFileTask3.bin и вывести на консоль. Округлить до трёх *");
+            Console.WriteLine("* знаков после запятой                                                        *");
+            Console.WriteLine("*******************************************************************************");
+            Console.WriteLine("* Исходные данные:                                                            *");
+            Console.WriteLine("*******************************************************************************");
+            int x = 5;
+            Console.WriteLine("* Результат:                                                                  *");
+            Console.WriteLine("*******************************************************************************");
+            string res = ds.SaveToFileTextData(x);
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан");
+            Console.ReadKey();
+        }
+    }
+}
