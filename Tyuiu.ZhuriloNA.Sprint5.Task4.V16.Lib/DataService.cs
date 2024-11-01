f06d1ac674c6c139389e3ed41c60f859eb802a81
Tyuiu.ZhuriloNA@outlook.com <121nikit121@gmail.com>
2024-11-01 19:48:20 +0500

    Task4

--- Tyuiu.ZhuriloNA.Sprint5.Task4.V16.Lib/DataService.cs
+++ Tyuiu.ZhuriloNA.Sprint5.Task4.V16.Lib/DataService.cs
@@ -1,15 +1,24 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using tyuiu.cources.programming.interfaces.Sprint5;
+
 namespace Tyuiu.ZhuriloNA.Sprint5.Task4.V16.Lib
 {
     public class DataService : ISprint5Task4V16
+{
+    public double LoadFromDataFile(string path)
     {
-        public double LoadFromDataFile(string path)
-        {
-            string strX = File.ReadAllText(path);
-            double res = Math.Cos(Convert.ToDouble(strX)) + Math.Pow(Convert.ToDouble(strX), 2) - (2 * Convert.ToDouble(strX)) / 1.2;
-            res  = Math.Round(res, 3);
-            return res;
-        }
+        // Проверка на существование файла
+        if (!File.Exists(path))
+            throw new FileNotFoundException("Data file not found at the specified path.");
+
+        string data = File.ReadAllText(path);
+        if (!double.TryParse(data, out double x))
+            throw new FormatException("The data in the file is not a valid double value.");
+
+        double y = Math.Cos(x) + Math.Pow(x, 2) - 2 * x / 1.2;
+
+        return Math.Round(y, 3);
     }
+}
 }
