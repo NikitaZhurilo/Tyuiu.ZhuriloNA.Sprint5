0935b51b051b56419e0e26bec3fdcf26a65d3ad8
Tyuiu.ZhuriloNA@outlook.com <121nikit121@gmail.com>
2024-11-22 22:30:40 +0500

    Task4

--- Tyuiu.ZhuriloNA.Sprint5.Task4.V16.Lib/DataService.cs
+++ Tyuiu.ZhuriloNA.Sprint5.Task4.V16.Lib/DataService.cs
@@ -1,24 +1,15 @@
-﻿using System;
-using System.IO;
+﻿
 using tyuiu.cources.programming.interfaces.Sprint5;
-
 namespace Tyuiu.ZhuriloNA.Sprint5.Task4.V16.Lib
 {
     public class DataService : ISprint5Task4V16
-{
-    public double LoadFromDataFile(string path)
     {
-        // Проверка на существование файла
-        if (!File.Exists(path))
-            throw new FileNotFoundException("Data file not found at the specified path.");
 
-        string data = File.ReadAllText(path);
-        if (!double.TryParse(data, out double x))
-            throw new FormatException("The data in the file is not a valid double value.");
-
-        double y = Math.Cos(x) + Math.Pow(x, 2) - 2 * x / 1.2;
-
-        return Math.Round(y, 3);
+        public double LoadFromDataFile(string path)
+        {
+            string strX = File.ReadAllText(path);
+            double res = Math.Round(Math.Cos(Convert.ToDouble(strX)) + Convert.ToDouble(strX) * Convert.ToDouble(strX) - 2 * Convert.ToDouble(strX) / 1.2, 3);
+            return res;
+        }
     }
-}
 }
--- Tyuiu.ZhuriloNA.Sprint5.Task4.V16.Test/DataServiceTest.cs
+++ Tyuiu.ZhuriloNA.Sprint5.Task4.V16.Test/DataServiceTest.cs
@@ -1,17 +1,16 @@
 using Tyuiu.ZhuriloNA.Sprint5.Task4.V16.Lib;
-namespace Tyuiu.ZhuriloNA.Sprint5.Task4.V16.Test
+namespace Tyuiu.ZhuriloNA.Sprint5.Task4.V16.Test;
+
+[TestClass]
+public class DataServiceTest
 {
-    [TestClass]
-    public class DataServiceTest
+    [TestMethod]
+    public void TestMethod1()
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-            string path = @"C:\Users\azhur\AppData\Local\Temp\DataSprint5\InPutDataFileTask4V16.txt";
-            FileInfo fileInfo = new FileInfo(path);
-            bool fileExists = fileInfo.Exists;
-            bool wait = true;
-            Assert.AreEqual(wait, fileExists);
-        }
+        string path = @"C:\Users\azhur\AppData\Local\Temp\DataSprint5\InPutDataFileTask4V16.txt";
+        FileInfo fileInfo = new FileInfo(path);
+        bool fileExists = fileInfo.Exists;
+        bool wait = true;
+        Assert.AreEqual(wait, fileExists);
     }
 }
