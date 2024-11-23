d62bd4411bb9c0df325d4af28dfe9052f78c36a8
Tyuiu.ZhuriloNA@outlook.com <121nikit121@gmail.com>
2024-11-24 00:12:35 +0500

    Task4

--- Tyuiu.ZhuriloNA.Sprint5.Task4.V16.Lib/DataService.cs
+++ Tyuiu.ZhuriloNA.Sprint5.Task4.V16.Lib/DataService.cs
@@ -7,9 +7,10 @@ namespace Tyuiu.ZhuriloNA.Sprint5.Task4.V16.Lib
 
         public double LoadFromDataFile(string path)
         {
-            string strX = File.ReadAllText(path);
-            double res = Math.Round(Math.Cos(Convert.ToDouble(strX)) + Convert.ToDouble(strX) * Convert.ToDouble(strX) - 2 * Convert.ToDouble(strX) / 1.2, 3);
-            return res;
+            string n = File.ReadAllText(path);
+            double x = double.Parse(n.Replace('.', ','));
+            double result = Math.Cos(x) + Math.Pow(x, 2) - 2 * x / 1.2;
+            return Math.Round(result, 3);
         }
     }
 }
