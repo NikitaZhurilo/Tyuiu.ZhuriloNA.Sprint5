7c7813aec2b2abb329aedc647b0a085ab41971e8
Tyuiu.ZhuriloNA@outlook.com <121nikit121@gmail.com>
2024-11-26 19:19:44 +0500

    Task6

---
+++ Tyuiu.ZhuriloNA.Sprint5.Task6.V23.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.ZhuriloNA.Sprint5.Task6.V23.Lib
+{
+    public class DataService : ISprint5Task6V23
+    {
+        public int LoadFromDataFile(string path)
+        {
+            int count = 0;
+            char[] chars = File.ReadAllText(path).ToCharArray();
+            for (int i = 0; i < chars.Length; i++) 
+            {
+                if (chars[i] == '-')
+                {
+                    count += 1;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ Tyuiu.ZhuriloNA.Sprint5.Task6.V23.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+using Tyuiu.ZhuriloNA.Sprint5.Task6.V23.Lib;
+namespace Tyuiu.ZhuriloNA.Sprint5.Task6.V23.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\azhur\AppData\Local\Temp\DataSprint5\InPutDataFileTask6V23.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
