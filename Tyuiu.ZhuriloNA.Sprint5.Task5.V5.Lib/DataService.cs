2f83c5e7e0c0c15d4edbafe382d63ea884bf053f
Tyuiu.ZhuriloNA@outlook.com <121nikit121@gmail.com>
2024-11-26 18:54:05 +0500

    Task5

--- Tyuiu.ZhuriloNA.Sprint5.Task5.V5.Lib/DataService.cs
+++ Tyuiu.ZhuriloNA.Sprint5.Task5.V5.Lib/DataService.cs
@@ -8,7 +8,11 @@ namespace Tyuiu.ZhuriloNA.Sprint5.Task5.V5.Lib
         {
             string n = File.ReadAllText(path);
             string[] array = n.Split(' ');
-            double[] x = Array.ConvertAll(array, double.Parse);
+            double[] x = new double[array.Length];
+            for (int j = 0; j < array.Length; j++)
+            {
+                x[j] = double.Parse(array[j].Replace('.', ','));
+            }
             double max = -10 * 10;
             double min = 10 * 10;
             for (int i = 0; i < array.Length; i++)
